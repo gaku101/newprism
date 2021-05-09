7d1a64cf14255555d7085df2cf81df0d1da4cfd0
takehiro <takehiro-r133@hotmail.co.jp>
2021-05-09 21:39:02 +0900

    add oneListItem component

--- components/itemList.tsx
+++ components/itemList.tsx
@@ -1,16 +1,49 @@
 import { useQuery } from "@apollo/client";
 import { BUNDLES_QUERY, FEEDS_QUERY } from "../utils/api/graphql/queries";
-import { BundleObject, FeedObject, ItemType } from "../utils/types";
+import {
+  BundleObject,
+  FeedObject,
+  ItemType,
+  SelectedFeedState,
+} from "../utils/types";
 import { NotifyError } from "./notifyError";
 import { NotifyLoading } from "./notifyLoading";
+import { OneListItem } from "./oneListItem";
+import { Dispatch, SetStateAction, useEffect } from "react";
 
-export const ItemList = ({ type }: { type: ItemType }) => {
+export const ItemList = ({
+  type,
+  selected,
+  setSelected,
+  useSelected = false,
+  allowEdits = false,
+}: {
+  type: ItemType;
+  selected?: SelectedFeedState;
+  setSelected?: Dispatch<SetStateAction<SelectedFeedState>>;
+  useSelected: boolean;
+  allowEdits?: false;
+}) => {
   const isFeed = type === ItemType.FeedType;
   const { loading, error, data } = useQuery(
     isFeed ? FEEDS_QUERY : BUNDLES_QUERY
   );
   const { feeds, bundles } = data || {};
   const itemList = isFeed ? feeds : bundles;
+
+  useEffect(() => {
+    (async () => {
+      if (useSelected && itemList && itemList.length && selected.id == null) {
+        const firstItem = itemList[0];
+        await setSelected({
+          id: firstItem.id,
+          feeds: isFeed ? [firstItem] : firstItem["feeds"],
+          editMode: false,
+          newMode: false,
+        });
+      }
+    })();
+  });
   if (loading) {
     return <NotifyLoading />;
   }
@@ -22,7 +55,15 @@ export const ItemList = ({ type }: { type: ItemType }) => {
       <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4">
         {itemList.length ? (
           itemList.map((item: FeedObject | BundleObject) => (
-            <p key={item.id}>{item.id}</p>
+            <OneListItem
+              type={type}
+              item={item}
+              key={item.id}
+              useSelected={useSelected}
+              allowEdits={allowEdits}
+              selected={selected}
+              setSelected={setSelected}
+            />
           ))
         ) : (
           <p>None are present. Why not add one?</p>
