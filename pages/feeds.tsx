109fde8562df52a2bb9c884b89b5987280a9e1bc
takehiro <takehiro-r133@hotmail.co.jp>
2021-05-10 16:38:32 +0900

    create items and item detail pages

---
+++ pages/feeds.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { ItemList } from "../components/itemList";
+import { Layout } from "../components/layout";
+import { ItemType, SelectedFeedState } from "../utils/types";
+
+const FeedsPage = () => {
+  const initialSelected: SelectedFeedState = {
+    id: null,
+    feeds: [],
+    editMode: false,
+    newMode: false,
+  };
+  const [selected, setSelected] = useState(initialSelected);
+  return (
+    <Layout>
+      <div className="grid grid-cols-2">
+        <h3 className="grif-cols-1 justify-start flex text-lr font-medium py-4">
+          Bundle page
+        </h3>
+      </div>
+      <ItemList
+        type={ItemType.FeedType}
+        useSelected={true}
+        allowEdits={true}
+        selected={selected}
+        setSelected={setSelected}
+      />
+    </Layout>
+  );
+};
+
+export default FeedsPage;
